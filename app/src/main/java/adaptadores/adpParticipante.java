9b6e7b73ba427087b80c58bbbbe33e22911767d8
DanielaMoreiraTorres <66782858+DanielaMoreiraTorres@users.noreply.github.com>
2021-01-09 16:18:53 -0500

    contacts

---
+++ app/src/main/java/adaptadores/adpParticipante.java
@@ -0,0 +1,90 @@
+package adaptadores;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplicationfin.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+import java.util.List;
+
+import clases.Participante;
+
+public class adpParticipante extends RecyclerView.Adapter<adpParticipante.ViewHolder> {
+
+    private List<Participante> datos;
+    public adpParticipante(List<Participante> datos){this.datos = datos;}
+
+    public static boolean showShimmer = true;
+    int cantShimmer = 8;
+    private View.OnClickListener listener;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ShimmerFrameLayout shimmerFrameLayout;
+        TextView txtParticipante;
+        ImageView btnShimmer;
+        TextView txtCel;
+        ImageView btnShimmer1;
+        TextView txtEmail;
+        ImageView btnShimmer2;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            shimmerFrameLayout = itemView.findViewById(R.id.shimmer);
+            txtParticipante = (TextView) itemView.findViewById(R.id.txtNombreAutor);
+            btnShimmer = itemView.findViewById(R.id.imageViewliPart);
+            txtCel = (TextView) itemView.findViewById(R.id.txtCelular);
+            btnShimmer1 = itemView.findViewById(R.id.imageViewliTel);
+            txtEmail = (TextView) itemView.findViewById(R.id.txtEmail);
+            btnShimmer2 = itemView.findViewById(R.id.imageViewliEm);
+        }
+
+        public void add_datos(Participante valor) {
+            txtParticipante.setText(valor.getNombre());
+            txtCel.setText(valor.getTelefono());
+            txtEmail.setText(valor.getEmail());
+        }
+    }
+
+    @NonNull
+    @Override
+    public adpParticipante.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = null;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_participante,null,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull adpParticipante.ViewHolder holder, int position) {
+        try {
+            if(showShimmer)
+            {
+                holder.shimmerFrameLayout.startShimmer();
+            }
+            else
+            {
+                holder.shimmerFrameLayout.stopShimmer();
+                holder.shimmerFrameLayout.setShimmer(null);
+                holder.txtParticipante.setBackground(null);
+                holder.txtCel.setBackground(null);
+                holder.txtEmail.setBackground(null);
+                holder.add_datos(datos.get(position));
+            }
+        }
+        catch (Exception e){
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        try{
+            return showShimmer ? cantShimmer : datos.size();
+        }catch (Exception e) {return cantShimmer;}
+    }
+}
--- app/src/main/java/com/example/myapplicationfin/activity_contacts.java
+++ app/src/main/java/com/example/myapplicationfin/activity_contacts.java
@@ -1,14 +1,96 @@
 package com.example.myapplicationfin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adaptadores.adpParticipante;
+import clases.Participante;
 
 public class activity_contacts extends AppCompatActivity {
+
+    RecyclerView rcvParticipantes;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        rcvParticipantes = findViewById(R.id.rcvParticipantes);
+        LinearLayoutManager linear = new LinearLayoutManager(getApplicationContext());
+        linear.setOrientation(LinearLayoutManager.VERTICAL);
+        rcvParticipantes.setLayoutManager(linear);
+        rcvParticipantes.hasFixedSize();
+
+        adpParticipante.showShimmer = true;
+
+        List<Participante> lista = new ArrayList<Participante>();
+        final adpParticipante adpParticipante = new adpParticipante(lista);
+        rcvParticipantes.setAdapter(adpParticipante);
+
+        Participante p1 = new Participante("Burbano Parraga Cristhian","+593980395656","cristhian.burbano2016@uteq.edu.ec");
+        Participante p2 = new Participante("Carvajal Florencia Carlos","+593979094938","carlos.carvajal2015@uteq.edu.ec");
+        Participante p3 = new Participante("Guerrero Moreira Rafael","+593979094938","rafael.guerrero2016@uteq.edu.ec");
+        Participante p4 = new Participante("Jaya Puruncaja Ruben","+593979094938","ruben.jaya2015@uteq.edu.ec");
+        Participante p5 = new Participante("Moreira Torres Daniela","+593979094938","daniela.moreira2015@uteq.edu.ec");
+        Participante p6 = new Participante("Ordoñez Guerrero Karina","+593979094938","karina.ordonez2016@uteq.edu.ec");
+        Participante p7 = new Participante("Taquez Suarez Aura","+593979094938","aura.taquez2015@uteq.edu.ec");
+        Participante p8 = new Participante("Zapata Espinoza Victor","+593979094938","victor.zapata2016@uteq.edu.ec");
+
+        final List<Participante> finalLista = new ArrayList<Participante>();
+        finalLista.add(p1);
+        finalLista.add(p2);
+        finalLista.add(p3);
+        finalLista.add(p4);
+        finalLista.add(p5);
+        finalLista.add(p6);
+        finalLista.add(p7);
+        finalLista.add(p8);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adpParticipante adpCreditos = new adpParticipante(finalLista);
+                rcvParticipantes.setAdapter(adpCreditos);
+                adpCreditos.showShimmer = false;
+                adpCreditos.notifyDataSetChanged();
+            }
+        }, 1000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menutoolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.btnInformacion) {
+            Intent intent = new Intent(this, activity_information.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        if(id == R.id.btnContactos) {
+            Intent intent = new Intent(this, activity_contacts.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
