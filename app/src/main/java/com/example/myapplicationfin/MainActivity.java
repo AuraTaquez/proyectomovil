352383b08d4ce55f9afb354427911d4f09678a26
DanielaMoreiraTorres <66782858+DanielaMoreiraTorres@users.noreply.github.com>
2021-01-09 14:53:37 -0500

    toolbar inicio

--- app/src/main/java/com/example/myapplicationfin/MainActivity.java
+++ app/src/main/java/com/example/myapplicationfin/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.myapplicationfin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView txtUser;
+    TextView txtPass;
+    Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        txtUser=(TextView) findViewById(R.id.et_usuario);
+        txtPass=(TextView) findViewById(R.id.contraseña);
+
+        btnIniciar=(Button) findViewById(R.id.iniciar_sesion);
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tipo = "A";
+                iniciar(tipo);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menutoolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.btnInformacion) {
+            Intent intent = new Intent(this, activity_information.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        if(id == R.id.btnContactos) {
+            Intent intent = new Intent(this, activity_contacts.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void iniciar(String tipo)
+    {
+        if(tipo.equals("A"))
+        {
+            Intent intent = new Intent(this, admin.class);
+            startActivity(intent);
+        }
+        else if (tipo.equals("T"))
+        {
+            Intent intent = new Intent(this, terapeuta.class);
+            startActivity(intent);
+        }
+        else if (tipo.equals("P"))
+        {
+            Intent intent = new Intent(this, paciente.class);
+            startActivity(intent);
+        }
     }
 }
