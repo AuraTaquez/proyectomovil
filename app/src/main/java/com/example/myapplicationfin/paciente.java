da764e722c3615a9c364c72f0c25f7f2806201ea
DanielaMoreiraTorres <66782858+DanielaMoreiraTorres@users.noreply.github.com>
2021-01-11 13:08:46 -0500

    paciente

--- app/src/main/java/com/example/myapplicationfin/paciente.java
+++ app/src/main/java/com/example/myapplicationfin/paciente.java
@@ -1,14 +1,88 @@
 package com.example.myapplicationfin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class paciente extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class paciente extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+    Toolbar toolbar;
+    Fragment fragment;
+    boolean fragmentTransaction;
+
+    //color de fondo de imagen: #C3DDF0
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paciente);
+
+        toolbar = findViewById(R.id.toolbar_paciente);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        fragmentTransaction = false;
+        fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.Pmenu_seccion_1:
+
+                break;
+            case R.id.Pmenu_seccion_2:
+
+                break;
+            case R.id.Pmenu_seccion_3:
+
+                break;
+        }
+        if(fragmentTransaction) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.contenido_paciente, fragment).commit();
+            menuItem.setChecked(true);
+            getSupportActionBar().setTitle(menuItem.getTitle());
+        }
+        drawerLayout.closeDrawers();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_paciente,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout_paciente);
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        if(id == R.id.btnCerrarSesion) {
+            Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
