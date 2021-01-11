f94a1b7f576c26371271a0fd9b32eeb2d8df457a
CarlosSebastianCarvajal <77211381+CarlosSebastianCarvajal@users.noreply.github.com>
2021-01-11 00:46:14 -0500

    Terapeuta

--- app/src/main/java/com/example/myapplicationfin/terapeuta.java
+++ app/src/main/java/com/example/myapplicationfin/terapeuta.java
@@ -1,8 +1,16 @@
 package com.example.myapplicationfin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 
 public class terapeuta extends AppCompatActivity {
 
@@ -10,5 +18,24 @@ public class terapeuta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terapeuta);
+
+        ImageButton info = findViewById(R.id.btnRegistrarPaciente);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), registrar_paciente.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        TextView registrarPaciente = findViewById(R.id.txtRegistrarPaciente);
+        registrarPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), registrar_paciente.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 }
