d7cd639e45848bf1ac15c95efd25d968c71175ff
DESKTOP-29SB55F\AURI <aura.taquez2015@uteq.edu.ec>
2021-01-12 00:52:12 -0500

    Admin

---
+++ app/src/main/java/com/example/myapplicationfin/Info_admin.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationfin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_admin);
+    }
+}
