405e878232e6999580df227ca3783f057407378a
CarlosSebastianCarvajal <77211381+CarlosSebastianCarvajal@users.noreply.github.com>
2021-02-02 21:57:46 -0500

    FireStore - Dispositivos

--- app/src/main/java/com/example/myapplicationfin/activity_dispositives.java
+++ app/src/main/java/com/example/myapplicationfin/activity_dispositives.java
@@ -6,14 +6,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 import java.util.ArrayList;
 
@@ -23,6 +31,7 @@ import clases.DispositiveModel;
 public class activity_dispositives extends AppCompatActivity {
 
     private DatabaseReference refDataBase;
+    private FirebaseFirestore refFireStore;
     EditText txtMensaje;
     Button btnEnviar;
 
@@ -36,12 +45,38 @@ public class activity_dispositives extends AppCompatActivity {
         setContentView(R.layout.activity_dispositivos);
 
         refDataBase = FirebaseDatabase.getInstance().getReference();
+        refFireStore = FirebaseFirestore.getInstance();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rcvDispositives);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
-        getDispositivosFromFireBase();
+        getDispositivosFromFireStore();
+    }
+
+    private void getDispositivosFromFireStore(){
+        refFireStore.collection("dispositivos")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            mDispositivesList.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String MAC = document.getString("mac");
+                                String dispositive = document.getString("device");
+                                String state = document.getString("estado");
+                                String observation = document.getString("observation");
+                                mDispositivesList.add(new DispositiveModel("0", MAC, dispositive, state, observation));
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                            mAdapter = new adpDispositive(mDispositivesList, R.layout.dispositivos_view);
+                            mRecyclerView.setAdapter(mAdapter);
+                        } else {
+                            //Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 
 
@@ -71,4 +106,7 @@ public class activity_dispositives extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
